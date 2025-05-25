4b576c8fc18e0a8a70370b2688c8d0cc890cf538
Mangus <manguseq4@gmail.com>
2025-05-25 21:38:10 +0200

    Bump version to 0.0.2 and enhance command handling in functions.rs

--- src/functions.rs
+++ src/functions.rs
@@ -1,47 +1,75 @@
 use colored::*;
 
-const _FUNCTIONS: [&str; 3] = ["hello", "help", "exit"];
+const _FUNCTIONS: [&str; 5] = ["hello", "help", "exit","whoami","say"];
 
 fn hello() {
-    println!("Hello!");
+    println!("{}","Hello! :)".bright_green().bold());
+}
+
+fn whoami() {
+    println!("{}",whoami::username().bright_blue().bold());
+}
+
+fn say(args: Vec<&str>) {
+    for (_, arg) in args.iter().enumerate() {
+        println!("{}", arg.trim().bright_yellow());
+    }
 }
 
 pub fn welcome() {
     print!("{}", "    Welcome to ".white());
     println!("{}", "[mter]".white().on_bright_magenta().bold());
+    println!("    Version: {}", env!("CARGO_PKG_VERSION").bright_blue().bold());
     println!("{}", "    Type 'help' for a list of commands.".bright_black());
 }
 
 
 pub fn handle(input: &str) {
-    if _FUNCTIONS.contains(&input) {
-        match input {
-            "hello" => hello(),
+    let trimmed = input.trim();
+    let mut parts = trimmed.split_whitespace();
+    let cmd = parts.next().unwrap_or("");
+    let args: Vec<&str> = parts.collect();
 
-            "help" => println!(
+    match cmd {
+        "hello" => hello(),
+
+        "help" => {
+            println!(
                 "{} {:?}",
                 "Available commands:".bright_black(),
                 _FUNCTIONS
-            ),
-
-            "exit" => {
-                println!("{}", "Exiting...".bright_black());
-                std::process::exit(0);
-            }
-
-            _ => println!(
+            );
+            println!(
                 "{} {}",
-                "Unknown command:".bright_black(),
-                input
-            ),
-        }
-        return;
-    }
+                "Version:".bright_black(),
+                env!("CARGO_PKG_VERSION").bright_green()
+            );
+        },
 
-    println!(
-        "{} {} {}",
-        "[functions.rs] Unknown command:".bright_black(),
-        input,
-        "type 'help' for a list of commands.".bright_black()
-    );
+        "exit" => {
+            println!("{}", "Exiting...".bright_black());
+            std::process::exit(0);
+        }
+        "whoami" => whoami(),
+
+        "say" => {
+            if args.is_empty() {
+                println!(
+                    "{}",
+                    "[functions.rs] 'say' functions needs at least one argument".bright_black()
+                );
+            } else {
+
+                let joined = args.join(" ");
+                let parts: Vec<&str> = joined.split('|').collect();
+                say(parts);
+            }
+        }
+        _ => println!(
+            "{} {} {}",
+            "[functions.rs] Unknown command:".bright_black(),
+            input,
+            "type 'help' for a list of commands.".bright_black()
+        ),
+    }
 }
